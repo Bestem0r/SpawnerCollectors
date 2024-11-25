3ffb8f58067bba6ab5e291e174e5fc021b3f1a05
Vebjorn Elvekrok <vebjorn.elvekrok@gmail.com>
2024-11-25 19:34:59 +0100

    Add cave spider is spider option

--- src/main/java/me/bestem0r/spawnercollectors/CustomEntityType.java
+++ src/main/java/me/bestem0r/spawnercollectors/CustomEntityType.java
@@ -1,5 +1,6 @@
 package me.bestem0r.spawnercollectors;
 
+import net.bestemor.core.config.ConfigManager;
 import org.bukkit.entity.EntityType;
 
 public class CustomEntityType {
@@ -10,12 +11,19 @@ public class CustomEntityType {
     private String customType;
 
     public CustomEntityType(EntityType type) {
-        this.entityType = type;
+        if (type == EntityType.CAVE_SPIDER && ConfigManager.getBoolean("cave_spider_is_spider")) {
+            entityType = EntityType.SPIDER;
+        } else {
+            this.entityType = type;
+        }
     }
 
     public CustomEntityType(String type) {
         if (type.equals("MUSHROOM")) {
             type = "MUSHROOM_COW";
+        }
+        if (type.equals("CAVE_SPIDER") && ConfigManager.getBoolean("cave_spider_is_spider")) {
+            type = "SPIDER";
         }
         if (isEntity(type.toUpperCase())) {
             this.entityType = EntityType.valueOf(type.toUpperCase());
